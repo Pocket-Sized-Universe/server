69340b818fdfe4b625e70fde6f29a2f9df2bb554
NostraThomas99 <34015422+NostraThomas99@users.noreply.github.com>
2025-09-07 06:50:51 -0500

    Add migration to update "hash" column in "torrent_file_entries" table to byte array type
    - Implements a migration to change the "hash" column in the "torrent_file_entries" table from string type to byte array type.
    - Includes updated designer file to match the schema changes.

---
+++ MareSynchronosServer/PocketSizedUniverseShared/Migrations/20250907115002_MigrateFromStringToByteArrayHashTake2.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PocketSizedUniverseServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateFromStringToByteArrayHashTake2 : Migration
+    {
+        /// <inheritdoc />
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(name: "hash", table: "torrent_file_entries", type: "bytea", nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "hash", table: "torrent_file_entries");
+        }
+    }
+}
